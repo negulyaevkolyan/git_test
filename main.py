aa8dd4d8d8c10dc81a29cf9119435b405c0094f4
lakustovdimka <lakustovdimka@gmail.com>
2025-07-14 12:53:20 +0500

    12345678

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import string
 
 
 def generate_password(length):
-    """Генерирует случ35а762йный пароль заданной длины."""
+    """Генерирует случ35а7628йный пароль заданной длины."""
     # Все возможные символы: буквы (нижний/верхний регистр), цифры, спецсимволы
     characters = string.ascii_letters + string.digits + string.punctuation
 

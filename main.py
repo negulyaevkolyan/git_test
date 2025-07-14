710a9faf2f5c26a48faf6c1f42c4bd09d8422f63
lakustovdimka <lakustovdimka@gmail.com>
2025-07-14 12:51:41 +0500

    1

---
+++ main.py
@@ -0,0 +1,25 @@
+import random
+import string
+
+
+def generate_password(length):
+    """Генерирует случайный пароль заданной длины."""
+    # Все возможные символы: буквы (нижний/верхний регистр), цифры, спецсимволы
+    characters = string.ascii_letters + string.digits + string.punctuation
+
+    # Генерация пароля
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+
+
+if __name__ == "__main__":
+    print("Генератор случайных паролей")
+    try:
+        pass_length = int(input("Введите желаемую длину пароля: "))
+        if pass_length <= 0:
+            print("Длина пароля должна быть положительным числом.")
+        else:
+            new_password = generate_password(pass_length)
+            print(f"Ваш сгенерированный пароль: {new_password}")
+    except ValueError:
+        print("Некорректный ввод. Пожалуйста, введите целое число для длины.")
